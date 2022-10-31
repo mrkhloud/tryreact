c206ca335a93ad3b94ce71c9f0b51e2640d757e9
mrkhloud <mrkhloud228xd@gmail.com>
2022-10-31 16:24:04 +0300

    'випрчв'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function App() {
+    const [likes, setLikes] = useState(0)
+    const [value, setValue] = useState('')
+
+    function increment () {
+        setLikes(likes + 1)
+    }
+
+    function decrement () {
+        setLikes(likes - 1)
+    }
+
+    function clear () {
+        setLikes(likes - likes)
+    }
+
     return (
         <div className="App">
-
+            <h2>{value}</h2>
+            <input type='text'
+                   value={value}
+                   onChange={event => setValue(event.target.value)}/>
+            <h1>Кол-во лайков - {likes}</h1>
+            <button onClick={increment}>Инкрементирует</button>
+            <button onClick={decrement}>Дикрементирует</button>
+            <button onClick={clear}>Онулировать кол-во лайков</button>
         </div>
     );
 }
